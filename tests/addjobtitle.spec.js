f7e31f7ce9221d6141993c014f66b5b2fed0aa31
Shalini <shalini3106shine@gmail.com>
2025-05-01 00:41:26 +0530

    playwright tests

---
+++ tests/addjobtitle.spec.js
@@ -0,0 +1,21 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('verify add job title with mandatry fields', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.locator("input[name='username']").fill("Admin")
+  await page.locator("input[type='password']").fill("admin123")
+  await page.locator("button[type='submit']").click();
+  await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+ await page.locator("//span[text()='Admin']").click();
+ await page.locator("//span[normalize-space(text())='Job']").click();
+ await page.locator("//a[normalize-space(text())='Job Titles']").click();
+ await page.locator("//button[contains(.,'Add')]").click();
+await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/admin/saveJobTitle");
+ await page.locator("(//input[@class='oxd-input oxd-input--active'])[2]").fill("test engineer");
+ await page.locator("//textarea[@placeholder='Type description here']").fill("engineer for test");
+ await page.locator("//button[@type='submit']").click();
+ await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList");
+});
+
+
---
+++ tests/swaglogin.spec.js
@@ -0,0 +1,35 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+import swaglogindata from"../testdata/swaplogin.json"
+
+test('Verify login with standard username', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/index.html');
+ await page.locator("//input[@data-test='username']").fill(swaglogindata.std_username)
+ await page.locator("//input[@data-test='password']").fill(swaglogindata.password)
+ await page.locator("//input[@type='submit']").click();
+  await expect(page).toHaveURL("https://www.saucedemo.com/v1/inventory.html");
+});
+
+test('Verify login with locked username', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/index.html');
+  await page.locator("//input[@data-test='username']").fill(swaglogindata.prob_username)
+  await page.locator("//input[@data-test='password']").fill(swaglogindata.password)
+  await page.locator("//input[@type='submit']").click();
+  await expect(page).toHaveURL("https://www.saucedemo.com/v1/inventory.html");
+});
+
+test('Verify login with problem username', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/index.html');
+  await page.locator("//input[@data-test='username']").fill(swaglogindata.loc_username)
+ await page.locator("input[type='password']").fill(swaglogindata.password)
+ await page.locator("//input[@type='submit']").click();
+await expect(page.locator("//div[@class='login-box']//h3[1]")).toBeVisible()
+});
+
+test('Verify login with performance username', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/index.html');
+  await page.locator("//input[@data-test='username']").fill(swaglogindata.perf_username)
+  await page.locator("//input[@data-test='password']").fill(swaglogindata.perf_username)
+  await page.locator("//input[@type='submit']").click();
+  await expect(page.locator("//div[@class='login-box']//h3[1]")).toBeVisible()
+});
